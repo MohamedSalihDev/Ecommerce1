7aecc852dd0d6785f65576edadf09be2abe9c9db
Mohamed Salih <Mohamed.Salih.Dev@gmail.com>
2021-02-14 12:55:19 -0700

    installed mongoose, connected mongoose to AtlasDB

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const mongoose = require("mongoose")
+const dotenv = require("dotenv");
+dotenv.config()
+//app
 const app = express();
-require("dotenv").config();
+//db
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+    .then(() => console.log("DB Connected"))
 
+//routes
 app.get('/', (req, res) => {
     res.send('hello from node updated')
 })
