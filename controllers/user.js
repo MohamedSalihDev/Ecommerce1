1a30296166d20f46336d718b892156afeaa5f56a
Mohamed Salih <Mohamed.Salih.Dev@gmail.com>
2021-02-15 12:30:29 -0700

    first signup route and bring from controllers user to routes user

--- controllers/user.js
+++ controllers/user.js
@@ -1,3 +1,20 @@
-exports.sayHi = (req, res) => {
-    res.json({ message: "hello there" })
-}
+const user = require('../models/user')
+
+exports.signup = (req, res) => {
+    console.log('req.body', req.body)
+    const user = new user(req.body)
+    user.save((err, user) => {
+        if (err) {
+            return res.status(400).json({
+                err
+            })
+        }
+        res.json({
+            user
+        });
+
+
+    });
+};
+
+
