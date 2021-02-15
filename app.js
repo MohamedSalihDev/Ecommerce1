2f0627a7cbd4c0fa130644bc559eba60a253757f
Mohamed Salih <Mohamed.Salih.Dev@gmail.com>
2021-02-15 12:15:25 -0700

    add cookie parser, add morgan with dev flag, bodyParser, and cookieParser to middlewares

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const mongoose = require("mongoose")
+const morgan = require('morgan');
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
 const dotenv = require("dotenv");
 // import routes
 const userRoutes = require('./routes/user.js')
@@ -9,7 +12,10 @@ const app = express();
 //db
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
     .then(() => console.log("DB Connected"))
-
+// middlewares
+app.use(morgan('dev'))
+app.use(bodyParser.json())
+app.use(cookieParser())
 //routes middleware
 app.use('/api', userRoutes)
 const port = process.env.PORT || 8000
