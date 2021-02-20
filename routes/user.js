f0d18eca32c1cc583a0450404d0db1104d3365b3
Mohamed Salih <Mohamed.Salih.Dev@gmail.com>
2021-02-20 14:51:16 -0700

    write signout and requireSignin middleware function in controller and export to routes, test route working

--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const router = express.Router();
-const { signup } = require('../controllers/user')
-const { signin } = require('../controllers/user')
+const { signup, signin, signout, requireSignin } = require('../controllers/user')
 const { userSignupValidator } = require('../validator/index')
 
 
 router.post("/signup", userSignupValidator, signup)
 router.post("/signin", signin)
+router.get('/signout', signout)
 
+router.get('/hello', requireSignin, (req, res) => {
+    res.send('Hello there')
+});
 
 module.exports = router;
